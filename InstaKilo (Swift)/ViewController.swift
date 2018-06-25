e844ea52983af8cea7dcca9bff1a657674a54c9e
WillChew <williamchew95@gmail.com>
2018-06-25 17:01:27 -0400

    Initial Commit

---
+++ InstaKilo (Swift)/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  InstaKilo (Swift)
+//
+//  Created by Will Chew on 2018-06-25.
+//  Copyright © 2018 Will Chew. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
